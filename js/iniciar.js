e5e728b7e97ad5752d06aab0af7e4c877a3d20be
Ismael <ismael.villarreal@hotmail.es>
2021-01-27 00:35:45 +0100

    tarea1_organizar1: pantalla bienvenida terminada

--- js/iniciar.js
+++ js/iniciar.js
@@ -1,7 +1,9 @@
 function iniciaJuegoNormal(){
+    document.querySelector("#bienvenida").classList.remove("visible");
     console.log('iniciamos normal');
 }
 
 function iniciaJuegoRelax(){
+    document.querySelector("#bienvenida").classList.remove("visible");
     console.log('iniciamos juego relax');
 }
